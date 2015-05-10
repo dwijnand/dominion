e838da181a9b0e36abbab485e644b393486676b9
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-10 17:59:54 +0100

    Add baseSet start.

--- src/main/scala/net/mox9/dominion/Card.scala
+++ src/main/scala/net/mox9/dominion/Card.scala
@@ -29,7 +29,7 @@ case object Copper extends TreasureCard { def cost = 0.coin ; def value = 1.coin
 case object Silver extends TreasureCard { def cost = 3.coin ; def value = 2.coin }
 case object Gold   extends TreasureCard { def cost = 6.coin ; def value = 3.coin }
 
-// 24 Estates, 12 Dutchies, 12 Provinces
+// 24 Estates, 12 Dutchies, (12 Gardens), 12 Provinces
 sealed trait VictoryCard extends Card { def value: VPoint }
 
 case object Estate   extends VictoryCard { def cost = 2.coin ; def value = 1.vp }
---
+++ src/main/scala/net/mox9/dominion/baseSet.scala
@@ -0,0 +1,101 @@
+package net.mox9.dominion
+
+case object Cellar extends Card {
+  def cost = 2.coin
+}
+
+case object Chapel extends Card {
+  def cost = 2.coin
+}
+
+case object Moat extends Card {
+  def cost = 2.coin
+}
+
+case object Chancellor extends Card {
+  def cost = 3.coin
+}
+
+case object Village extends Card {
+  def cost = 3.coin
+}
+
+case object Woodcutter extends Card {
+  def cost = 3.coin
+}
+
+case object Workshop extends Card {
+  def cost = 3.coin
+}
+
+case object Bureaucrat extends Card {
+  def cost = 4.coin
+}
+
+case object Feast extends Card {
+  def cost = 4.coin
+}
+
+case object Gardens extends Card {
+  def cost = 4.coin
+}
+
+case object Militia extends Card {
+  def cost = 4.coin
+}
+
+case object Moneylender extends Card {
+  def cost = 4.coin
+}
+
+case object Remodel extends Card {
+  def cost = 4.coin
+}
+
+case object Smithy extends Card {
+  def cost = 4.coin
+}
+
+case object Spy extends Card {
+  def cost = 4.coin
+}
+
+case object Thief extends Card {
+  def cost = 4.coin
+}
+
+case object ThroneRoom extends Card {
+  def cost = 4.coin
+}
+
+case object CouncilRoom extends Card {
+  def cost = 5.coin
+}
+
+case object Festival extends Card {
+  def cost = 5.coin
+}
+
+case object Laboratory extends Card {
+  def cost = 5.coin
+}
+
+case object Library extends Card {
+  def cost = 5.coin
+}
+
+case object Market extends Card {
+  def cost = 5.coin
+}
+
+case object Mine extends Card {
+  def cost = 5.coin
+}
+
+case object Witch extends Card {
+  def cost = 5.coin
+}
+
+case object Adventurer extends Card {
+  def cost = 6.coin
+}
