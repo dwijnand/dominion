1707d6d189df668d13dbf36e2280e3875d814b00
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-10 16:57:31 +0100

    flatMaybe/maybeUnit.

--- src/main/scala/net/mox9/dominion/stdPimps.scala
+++ src/main/scala/net/mox9/dominion/stdPimps.scala
@@ -3,7 +3,9 @@ package net.mox9.dominion
 case class AnyW[A](private val x: A) extends AnyVal {
   def toUnit(): Unit = ()
 
-  def maybe[B](pf: A ?=> B): Option[B] = pf lift x
+  def maybe[B](pf: A ?=> B): Option[B]             = pf lift x
+  def flatMaybe[B](pf: A ?=> Option[B]): Option[B] = pf lift x flatten
+  def maybeUnit(pf: A ?=> Unit): Unit              = (pf lift x).getOrElse(())
 
   @inline def sideEffect(body: Unit): A = x
   @inline def doto(f: A => Unit): A     = sideEffect(f(x))
