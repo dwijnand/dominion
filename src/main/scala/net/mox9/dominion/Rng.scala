ddd9f073f53627fad6b31feb7182df186da7831f
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-13 00:33:31 +0100

    Add Rng.nextInt(Int).

--- src/main/scala/net/mox9/dominion/Rng.scala
+++ src/main/scala/net/mox9/dominion/Rng.scala
@@ -1,8 +1,27 @@
 package net.mox9.dominion
 
 case class Rng(seed: Long) extends AnyVal {
-  def nextInt: Int -> Rng = {
+  def nextBits(bits: Int): Int -> Rng = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-    (newSeed >>> 16).toInt -> Rng(newSeed)
+    (newSeed >>> (48 - bits)).toInt -> Rng(newSeed)
+  }
+
+  def nextInt: Int -> Rng = nextBits(32)
+
+  def nextInt(bound: Int): Int -> Rng = {
+    val n -> rng = nextBits(31)
+    val m = bound - 1
+    if ((bound & m) == 0)  // i.e., bound is a power of 2
+      ((bound * n.toLong) >> 31).toInt -> rng
+    else {
+      @tailrec def go(s: Int -> Rng): Int -> Rng = {
+        val n -> rng = s
+        val r = n % bound
+        if (n - r + m < 0)
+          go(rng nextBits 31)
+        else r -> rng
+      }
+      go(n -> rng)
+    }
   }
 }
