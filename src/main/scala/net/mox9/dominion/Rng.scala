6e7b1a5a440861440c98f3585e3af5bae1fc7d46
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-13 00:43:01 +0100

    Add Rng.shuffle for collections.

--- src/main/scala/net/mox9/dominion/Rng.scala
+++ src/main/scala/net/mox9/dominion/Rng.scala
@@ -1,5 +1,9 @@
 package net.mox9.dominion
 
+import scala.language.higherKinds
+
+import scala.collection.mutable.ArrayBuffer
+
 case class Rng(seed: Long) extends AnyVal {
   def nextBits(bits: Int): Int -> Rng = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
@@ -23,5 +27,23 @@ case class Rng(seed: Long) extends AnyVal {
       }
       go(n -> rng)
     }
+  }
+
+  def shuffle[T, CC[X] <: TraversableOnce[X]](xs: CC[T])(implicit bf: CBF[CC[T], T, CC[T]]): CC[T] -> Rng = {
+    val buf = new ArrayBuffer[T] ++= xs
+
+    def swap(i1: Int, i2: Int): Unit = {
+      val tmp = buf(i1)
+      buf(i1) = buf(i2)
+      buf(i2) = tmp
+    }
+
+    val rng = (buf.length to 2 by -1).foldLeft(this) { (rng, n) =>
+      val k -> rng2 = rng nextInt n
+      swap(n - 1, k)
+      rng2
+    }
+
+    (bf(xs) ++= buf).result() -> rng
   }
 }
