b200955e1c3a66562ae4666f5ad002eb23ea60fd
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-13 01:24:33 +0100

    Add PlayerState.

---
+++ src/main/scala/net/mox9/dominion/PlayerState.scala
@@ -0,0 +1,80 @@
+package net.mox9.dominion
+
+trait PlayerView {
+  def deck        : Deck
+  def handSize    : Int
+  def discardPile : DiscardPile
+
+  def actions : Actions
+  def buys    : Buys
+  def coins   : Coins
+}
+
+case class PlayerState(
+  deck        : Deck,
+  hand        : Vector[Card],
+  discardPile : DiscardPile,
+
+  actions : Actions,
+  buys    : Buys,
+  coins   : Coins,
+
+  rng: Rng
+) extends PlayerView {
+
+  def handSize = hand.size
+
+  def draw(n: Int): PlayerState =
+    (0 to n).foldLeft(this) { (s, _) =>
+      deck.draw match {
+        case Some(card -> remDeck) => s.copy(remDeck, hand :+ card)
+        case None                  =>
+          val newDeck -> newRng = discardPile newDeck rng
+          newDeck.draw match {
+            case Some(card -> remDeck) => s.copy(remDeck, hand :+ card, DiscardPile.empty, rng = newRng)
+            case None                  => s.copy(newDeck, hand,         DiscardPile.empty, rng = newRng)
+          }
+      }
+  }
+}
+
+class Deck private (cards: List[Card]) {
+  def size: Int        = cards.size
+  def isEmpty: Boolean = cards.isEmpty
+
+  def draw: Option[Card -> Deck] =
+    cards match {
+      case Nil    => None
+      case h :: t => h -> new Deck(t) some
+    }
+
+  // TODO reveal
+}
+object Deck {
+  def shuffleNew(cards: List[Card], rng: Rng): Deck -> Rng = {
+    val shuffledCards -> rng2 = rng shuffle cards
+    new Deck(shuffledCards) -> rng2
+  }
+}
+
+// drawing reduces by 1
+// what about when empty?
+
+//case class Hand ??
+
+class DiscardPile private (cards: List[Card]) {
+  def topCard: Option[Card] = cards.headOption
+  def isEmpty: Boolean = topCard.isEmpty
+
+  def ::(c: Card): DiscardPile        = new DiscardPile(c :: cards)
+  def :::(cs: Seq[Card]): DiscardPile = new DiscardPile(cs.toList ::: cards)
+
+  def newDeck(rng: Rng): Deck -> Rng = ???
+}
+object DiscardPile {
+  def empty: DiscardPile = new DiscardPile(Nil)
+}
+// appending adds to topCard
+// appending multiple adds to topCard
+
+//trait PosInt
