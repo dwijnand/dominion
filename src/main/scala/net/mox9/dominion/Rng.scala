db9feb835b8cf089525e4b8a40353de5633f4d7b
Dale Wijnand <dale.wijnand@gmail.com>
2015-05-21 09:07:48 +0100

    Add Rng.choose.

--- src/main/scala/net/mox9/dominion/Rng.scala
+++ src/main/scala/net/mox9/dominion/Rng.scala
@@ -40,6 +40,8 @@ case class Rng(seed: Long) extends AnyVal {
     }
   }
 
+  def choose[A](xs0: Trav[A]): A -> Rng = xs0.toIndexedSeq pipe (xs => chooseInt(0, xs.size) mapFst xs)
+
   def shuffle[T, CC[X] <: TraversableOnce[X]](xs: CC[T])(implicit bf: CBF[CC[T], T, CC[T]]): CC[T] -> Rng = {
     val buf = new ArrayBuffer[T] ++= xs
 
